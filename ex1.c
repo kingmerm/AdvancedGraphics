463d308e5ffbe4d9c599d727fd7ddbfa54e8250c
Matt King <Matt King>
2016-12-07 14:56:56 +0000

    grass growing and menu added for changing gravity, particle size etc

--- ex1.c
+++ ex1.c
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <math.h>
+#include <windows.h>
 
 #ifdef MACOSX
   #include <GLUT/glut.h>
@@ -23,14 +24,11 @@
 #endif
 
 #define DEG_TO_RAD_CONV 0.017453293f
-#define TIME_STEP .000005f
-#define INITIAL_VELOCITY 1.0f
-#define PARTICLE_SIZE 1.0f
-#define PARTICLE_NUMBER 30000000
-#define PARTICLE_LIFE .001f
+#define PARTICLE_NUMBER 1000000
+#define PARTICLE_LIFE .00001f
 #define EMITTER_NUMBER 20
-#define GRAVITY_CONST -1.5f
-#define GRASS_GROWTH_RATE 0.00001
+#define GRASS_GROWTH_RATE 0.0001
+#define MAXIMUM_GRASS_HEIGHT 2.0
 
 // Global viewpoint/camera variables 
 GLfloat latitude, longitude;
@@ -48,6 +46,19 @@ int axisEnabled= 1;
 GLfloat currentTime;
 
 GLfloat MOVEMENT_FACTOR = 2.5f;
+GLfloat TIME_STEP = .000005f;
+GLfloat GRAVITY_CONST = -2.0f;
+GLfloat PARTICLE_SIZE = 1.0f;
+static int rootMenuId;
+static int subAxisMenuId;
+static int subTerrainMenuId;
+static int gravitySubMenuId;
+static int emitterPositionSubMenuId;
+static int fountainSubMenuId;
+static int particleSizeSubMenuId;
+static int window;
+static int menuOpen = 0;
+static int val = 0;
 typedef struct
 {
 	GLfloat x;
@@ -74,6 +85,8 @@ typedef struct
   GLfloat particleTime;
   Color* color;
   GLfloat grassLength;
+  GLfloat grassColor;
+  GLfloat chanceOfFlowering;
   struct Particle* next;
 }Particle;
 
@@ -113,7 +126,7 @@ void calculateNewYPosition(Particle* p, GLfloat dt){
 		p->velocity->x *= 0.99;
 		p->velocity->z *= 0.99;
 	}
-    p->nextPos->y = p->velocity->y + (float)(1/2*(p->acceleration->y)*pow(dt,2));
+    p->nextPos->y = p->velocity->y + (float)(1/2*(GRAVITY_CONST)*pow(dt,2));
 	p->prevPos->y = p->currentPos->y;
 	p->currentPos->y += p->nextPos->y;
 	p->nextPos->y = 0.0f;
@@ -157,36 +170,36 @@ void initParticle(Particle* particle) {
 	particle->currentPos->y =emitter->position->y;
 	particle->currentPos->z =emitter->position->z;
 	if (myRandom() < 0.5) {
-		particle->velocity->x = -10.0f*myRandom();
+		particle->velocity->x = -4.0f*myRandom();
 	}
 	else
 	{
-		particle->velocity->x = 10.0f*myRandom();
+		particle->velocity->x = 4.0f*myRandom();
 	}
-	particle->velocity->y = 0.008;
+	particle->velocity->y = 0.025;
 	if (myRandom() < 0.5) {
-		particle->velocity->z = -10.0f*myRandom();
+		particle->velocity->z = -4.0f*myRandom();
 	}
 	else
 	{
-		particle->velocity->z = 10.0f*myRandom();
+		particle->velocity->z = 4.0f*myRandom();
 	}
-	particle->acceleration->x = .30f;
+	particle->acceleration->x = 3.0f;
 	particle->acceleration->y = GRAVITY_CONST;
-	particle->acceleration->z = .30f;
+	particle->acceleration->z = 3.0f;
 	particle->particleTime = .0f;
 	particle->next = NULL;
-	particle->color->G = (GLdouble)myRandom();
-	particle->color->B = (GLdouble)myRandom();
-	if (particle->color->G == 1.0 && particle->color->B == 1.0) {
+	particle->color->G = 0.0f;
+	particle->color->B = myRandom()*2.0;
+	particle->color->R = 0.0f;
+	if (particle->color->B == 1.0) {
 		particle->color->R = 1.0f;
-	}
-	else
-	{
-		particle->color->R = 0.0f;
+		particle->color->G = 1.0f;
 	}
 	particle->color->A = 1.0f;
+	particle->grassColor = (GLfloat) myRandom();
 	particle->grassLength = 0.01f*myRandom();
+	particle->chanceOfFlowering = myRandom();
 }
 ///////////////////////////////////////////////
 void initEmitter(Emitter* emitter) {
@@ -238,30 +251,41 @@ void drawParticles() {
 				glColor4f(tmp->color->R, tmp->color->G, tmp->color->B, tmp->color->A);
 				glVertex3f(tmp->currentPos->x, tmp->currentPos->y, tmp->currentPos->z);
 				glEnd();
-				if (tmp->color->A > 0) {
+				if (tmp->particleTime > PARTICLE_LIFE && tmp->color->A > 0) {
 					tmp->color->A -= .0001f;
 				}
 			}
 			else
 			{
-				glBegin(GL_LINES);
-				glVertex3f(tmp->currentPos->x, tmp->currentPos->y, tmp->currentPos->z);
-				if (tmp->grassLength > 0.7) {
-					glColor4f(.8, 0.0, 0.0, 0.9);
+				if (abs(tmp->currentPos->x) < 5 && abs(tmp->currentPos->z) < 5)
+				{
+					glBegin(GL_POINTS);
+					glColor4f(1.0, 1.0, tmp->color->B, tmp->color->A);
+					glVertex3f(tmp->currentPos->x, tmp->currentPos->y, tmp->currentPos->z);
+					glEnd();
+					if (tmp->color->A > 0) {
+						tmp->color->A -= .0075f;
+					}
 				}
 				else
 				{
-					glColor4f(0.0, tmp->color->G, 0.0, 0.75);
+					glBegin(GL_LINES);
+					glVertex3f(tmp->currentPos->x, tmp->currentPos->y, tmp->currentPos->z);
+					if (tmp->grassLength > 0.2 && tmp->chanceOfFlowering < 0.1) {
+						glColor4f(.8, tmp->grassColor, 0.0, 0.9);
+					}
+					else
+					{
+						glColor4f(0.0, tmp->grassColor, 0.0, 0.6);
+					}
+
+					glVertex3f(tmp->currentPos->x + 0.01*cos(myRandom()), tmp->currentPos->y + tmp->grassLength, tmp->currentPos->z);
+					glEnd();
+					if (tmp->grassLength < MAXIMUM_GRASS_HEIGHT)
+						tmp->grassLength += GRASS_GROWTH_RATE;
 				}
-				
-				glVertex3f(tmp->currentPos->x+0.01*cos(myRandom()), tmp->currentPos->y+tmp->grassLength, tmp->currentPos->z);
-				glEnd();
-				if (tmp->color->A > 0) {
-					//tmp->color->A -= .00000001f;
-				}
-				tmp->grassLength += GRASS_GROWTH_RATE;
 			}
-			tmp->particleTime += TIME_STEP; //need to make particle time variable and global time variable separate
+			tmp->particleTime += TIME_STEP;
 			emitter->particleSet[i] = tmp;
 			tmp = tmp->next;
 			i++;
@@ -270,15 +294,18 @@ void drawParticles() {
 		glutSwapBuffers();
 }
 //////////////////////////////////////////////
+int leftMouseDown = 0;
 void mouseMotion(int x, int y) {
 	// Called when mouse moves
-	xRotation += (y - mouseY);	mouseY = y;
-	yRotation += (x - mouseX);	mouseX = x;
-	// keep all rotations between 0 and 360.
-	if (xRotation > 360.0) xRotation -= 360.0;
-	if (xRotation < 0.0)   xRotation += 360.0;
-	if (yRotation > 360.0) yRotation -= 360.0;
-	if (yRotation < 0.0)   yRotation += 360.0;
+	if(leftMouseDown) {
+		xRotation += (y - mouseY);	mouseY = y;
+		yRotation += (x - mouseX);	mouseX = x;
+		// keep all rotations between 0 and 360.
+		if (xRotation > 360.0) xRotation -= 360.0;
+		if (xRotation < 0.0)   xRotation += 360.0;
+		if (yRotation > 360.0) yRotation -= 360.0;
+		if (yRotation < 0.0)   yRotation += 360.0;
+	}
 	// ask for redisplay
 	glutPostRedisplay();
 }
@@ -287,19 +314,17 @@ void mouseMotion(int x, int y) {
 void mousePress(int button, int state, int x, int y) {
 	// When left mouse button is pressed, save the mouse(x,y)
 	if ((button == GLUT_LEFT_BUTTON) && (state == GLUT_DOWN)) {
+		leftMouseDown = 1;
 		mouseX = x;
 		mouseY = y;
 	}
+	else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP)
+	{
+		leftMouseDown = 0;
+	}
 }
-int i;
 void display()
 {
-  glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
-  glViewport(0, 0, (GLsizei)800, (GLsizei)600);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluPerspective(60, (GLfloat)800 / (GLfloat)600, 1.0, 10000.0);
-  glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
   glRotatef(xRotation, 1.0, 0.0, 0.0);
   glRotatef(yRotation, 0.0, 1.0, 0.0);
@@ -310,31 +335,44 @@ void display()
             upX, upY, upZ);
   // Clear the screen
   glClear(GL_COLOR_BUFFER_BIT);
-  // If enabled, draw coordinate axis
-  if(axisEnabled) glCallList(axisList);
-  //0.52 green 0.37 blue 0.26
+  if (val != 5) {
 	  glBegin(GL_QUADS);
 	  glColor3f(0.52f, 0.37f, 0.26f);
-	  glVertex3f(-100.0, 0.0 ,-100.0);
+	  glVertex3f(-100.0, 0.0, -100.0);
 	  glVertex3f(-100.0, 0.0, 100.0);
 	  glVertex3f(100.0, 0.0, 100.0);
 	  glVertex3f(100.0, 0.0, -100.0);
 	  glEnd();
-	  if (emitter->particleSet[emitter->currentParticles - 1]->particleTime <= PARTICLE_LIFE && emitter->currentParticles < PARTICLE_NUMBER) {
+	  glBegin(GL_QUADS);
+	  glColor4f(0.0, .16f, 0.8f, .4f);
+	  glVertex3f(-5.0, 0.0, -5.0);
+	  glColor4f(0.0, .16f, .5f, .8f);
+	  glVertex3f(-5.0, 0.0, 5.0);
+	  glColor4f(0.0, 0.0, .70f, .4f);
+	  glVertex3f(5.0, 0.0, 5.0);
+	  glColor4f(1.0f, 1.0f, 1.0f, .8f);
+	  glVertex3f(5.0, 0.0, -5.0);
+	  glEnd();
+  }
+  //check to see if fountain is on
+  if (val != 14/*fountain is on*/) {
+	  if (emitter->currentParticles < PARTICLE_NUMBER) {
 		  //Add new particle
-		 emitter->particleSet[emitter->currentParticles - 1]->next = (Particle*)malloc(sizeof(Particle));
+		  emitter->particleSet[emitter->currentParticles - 1]->next = (Particle*)malloc(sizeof(Particle));
 		  //initialise new particle
 		  initParticle(emitter->particleSet[emitter->currentParticles - 1]->next);
 		  //increase number of particles
-		 emitter->currentParticles += 1;
+		  emitter->currentParticles += 1;
 	  }
+  }
+	// If enabled, draw coordinate axis
+	if (axisEnabled) glCallList(axisList);
     drawParticles();
 	glPopMatrix();
 	glutPostRedisplay();
 }
 
 ///////////////////////////////////////////////
-int refreshingEmitter;
 void keyboard(unsigned char key, int x, int y)
 {
  switch (key)
@@ -376,7 +414,6 @@ void keyboard(unsigned char key, int x, int y)
 	}
     break;
  }
- printf("center[X,Y,Z] = [%f,%f,%f]", centerX, centerY, centerZ);
   glutPostRedisplay();
 }
 
@@ -413,6 +450,139 @@ void makeAxes() {
   glEndList();
 }
 ///////////////////////////////////////////////
+void menu(int value) {
+	menuOpen = 1;
+	switch (value) {
+	case 0:
+	{
+		glutDestroyWindow(window);
+		exit(0);
+		glutPostRedisplay();
+		break;
+	}
+	case 2: {
+		axisEnabled = 1;
+		glutPostRedisplay();
+		printf("AXIS SHOWING \n");
+		break;
+	}
+	case 3:
+	{
+		axisEnabled = 0;
+		glutPostRedisplay();
+		printf("AXIS REMOVED \n");
+		break;
+	}
+	case 6: {
+		GRAVITY_CONST *= 0.25f;
+		glutPostRedisplay();
+		printf("GRAVITY IS: %f \n", GRAVITY_CONST);
+		break;
+	}
+	case 7: {
+		GRAVITY_CONST *= 0.5f;
+		printf("GRAVITY IS: %f \n", GRAVITY_CONST);
+		glutPostRedisplay();
+		break;
+	}
+	case 8: {
+		GRAVITY_CONST *= 1.5;
+		printf("GRAVITY IS: %f \n", GRAVITY_CONST);
+		glutPostRedisplay();
+		break;
+	}
+	case 9: {
+		GRAVITY_CONST *= 2.0;
+		printf("GRAVITY IS: %f \n", GRAVITY_CONST);
+		glutPostRedisplay();
+		break;
+	}
+	case 10:
+	{
+		GRAVITY_CONST = -2.0;
+		printf("GRAVITY RESET, GRAVITY IS: %f \n", GRAVITY_CONST);
+		glutPostRedisplay();
+		break;
+	}
+	case 11:
+	{
+		initEmitter(emitter);
+		emitter->position->x = 10.0f*myRandom();
+		emitter->position->y = 10.0f*myRandom();
+		emitter->position->z = 10.0f*myRandom();
+		initParticle(emitter->particleSet[0]);
+		glutPostRedisplay();
+		break;
+	}
+	case 12:
+	{
+		initEmitter(emitter);
+		emitter->position->x = 0.0f;
+		emitter->position->y = 0.0f;
+		emitter->position->z = 0.0f;
+		initParticle(emitter->particleSet[0]);
+		glutPostRedisplay();
+		break;
+	}
+	case 15:{
+		PARTICLE_SIZE = 1.0f;
+		glutPostRedisplay();
+		printf("PARTICLE SIZE IS: %f \n", PARTICLE_SIZE);
+		break;
+	}
+	case 16: {
+		PARTICLE_SIZE = 3.0f;
+		glutPostRedisplay();
+		printf("PARTICLE SIZE IS: %f \n", PARTICLE_SIZE);
+		break;
+	}
+	case 17: {
+		PARTICLE_SIZE = 5.0f;
+		glutPostRedisplay();
+		printf("PARTICLE SIZE IS: %f \n", PARTICLE_SIZE);
+		break;
+	}
+	default:
+	{
+		val = value;
+		glutPostRedisplay();
+		break;
+	}
+	}
+}
+void createMenu(void) {
+	subAxisMenuId = glutCreateMenu(menu);
+	glutAddMenuEntry("Show", 2);
+	glutAddMenuEntry("Remove", 3);
+	subTerrainMenuId = glutCreateMenu(menu);
+	glutAddMenuEntry("Show", 4);
+	glutAddMenuEntry("Remove", 5);
+	gravitySubMenuId = glutCreateMenu(menu);
+	glutAddMenuEntry("x0.25", 6);
+	glutAddMenuEntry("x0.5", 7);
+	glutAddMenuEntry("x1.5", 8);
+	glutAddMenuEntry("x2.0", 9);
+	glutAddMenuEntry("RESET", 10);
+	emitterPositionSubMenuId = glutCreateMenu(menu);
+	glutAddMenuEntry("Random", 11);
+	glutAddMenuEntry("Origin", 12);
+	fountainSubMenuId = glutCreateMenu(menu);
+	glutAddMenuEntry("Start", 13);
+	glutAddMenuEntry("Stop", 14);
+	particleSizeSubMenuId = glutCreateMenu(menu);
+	glutAddMenuEntry("Small", 15);
+	glutAddMenuEntry("Medium", 16);
+	glutAddMenuEntry("Large", 17);
+	rootMenuId = glutCreateMenu(menu);
+	glutAddMenuEntry("QUIT", 0);
+	glutAddSubMenu("Axis", subAxisMenuId);
+	glutAddSubMenu("Fountain", fountainSubMenuId);
+	glutAddSubMenu("Ground & Water", subTerrainMenuId);
+	glutAddSubMenu("Gravity", gravitySubMenuId);
+	glutAddSubMenu("Move Emitter", emitterPositionSubMenuId);
+	glutAddSubMenu("Particle Size", particleSizeSubMenuId);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
 void initGraphics(int argc, char *argv[])
 {
   /*INITIAL NAVIGATIONAL VARIABLES*/
@@ -458,15 +628,17 @@ void initGraphics(int argc, char *argv[])
   glutInitWindowSize(800, 600);
   glutInitWindowPosition(100, 100);
   glutInitDisplayMode(GLUT_DOUBLE);
-  glutCreateWindow("COMP37111 Particles");
+  window = glutCreateWindow("COMP37111 Particles");
   //Enable alpha blending
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   glEnable(GL_BLEND);
+  glShadeModel(GL_SMOOTH);
   glutDisplayFunc(display);
   glutKeyboardFunc(keyboard);
   glutMouseFunc(mousePress);
   glutMotionFunc(mouseMotion);
   glutReshapeFunc(reshape);
+  createMenu();
   makeAxes();
 }
 
